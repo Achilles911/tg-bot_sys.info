7d984d67f94fcbb981c168b66383e60bf7986428
Achilles911 <lolololo.lolo12345@mail.ru>
2024-09-24 22:06:53 +0300

    Добавлен вывод ресурсов кнопкой /rosources

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
 
@@ -38,8 +39,6 @@ func main() {
 
 		header := fmt.Sprintf("%-3s %-2s %-5s %-4s %-5s %-1s %-1s %-0s %-s\n", "PID", "PPID", "USER", "STAT", "VSZ", "%VSZ", "CPU", "%CPU", "COMMAND")
 
-		// Фильтр строки по отслеживаемым процессам
-
 		var filteredLines []string
 		for _, line := range lines {
 			for _, process := range monitoredProcesses {
@@ -55,9 +54,8 @@ func main() {
 		}
 		var formattedOutput []string
 		for _, line := range filteredLines {
-			fields := strings.Fields(line) // Разделяем строку на части (столбцы)
-			if len(fields) >= 10 {         // Убедимся, что есть минимум 10 столбцов
-				// Форматируем строку по размерам столбцов
+			fields := strings.Fields(line)
+			if len(fields) >= 10 {
 				formattedLine := fmt.Sprintf("%-6s %-8s %-8s %-6s %-8s %-5s %-4s %-4s %-s",
 					fields[0],                      // PID
 					fields[1],                      // PPID
@@ -72,7 +70,6 @@ func main() {
 			}
 		}
 
-		//Процессы в строку
 		output = header + strings.Join(formattedOutput, "\n")
 
 		// Ограничиваем длину сообщения
@@ -80,8 +77,56 @@ func main() {
 			output = output[:4000] + "\n...Output truncated..."
 		}
 
-		// Отправляем результат
 		return c.Send("Запущенные процессы:\n" + output)
+	})
+	b.Handle("/resources", func(c tele.Context) error {
+
+		topOut, err := exec.Command("top", "-b", "-n", "1").Output() // CPU
+		if err != nil {
+			return c.Send("Ошибка при получении данных о CPU")
+		}
+		freeOut, err := exec.Command("free", "-b").Output() // память
+		if err != nil {
+			return c.Send("Ошибка при получении данных о памяти")
+		}
+
+		lines := strings.Split(string(topOut), "\n")
+		var cpuLine string
+		for _, line := range lines {
+			if strings.Contains(line, "CPU:") {
+				cpuLine = line
+				break
+			}
+		}
+
+		freeLines := strings.Split(string(freeOut), "\n")
+		var memLine string
+		for _, line := range freeLines {
+			if strings.Contains(line, "Mem:") {
+				memLine = line
+				break
+			}
+		}
+
+		memFields := strings.Fields(memLine)
+		if len(memFields) >= 7 {
+			usedMem, _ := strconv.ParseFloat(memFields[1], 64)
+			freeMem, _ := strconv.ParseFloat(memFields[2], 64)
+			buffMem, _ := strconv.ParseFloat(memFields[4], 64)
+			cachedMem, _ := strconv.ParseFloat(memFields[5], 64)
+
+			usedMemMB := usedMem / (1024 * 1024)
+			freeMemMB := freeMem / (1024 * 1024)
+			buffMemMB := buffMem / (1024 * 1024)
+			cachedMemMB := cachedMem / (1024 * 1024)
+
+			memLine = fmt.Sprintf("Mem: %.2f MB used, %.2f MB free, %.2f MB buff, %.2f MB cached",
+				usedMemMB, freeMemMB, buffMemMB, cachedMemMB)
+		}
+
+		finalOutput := "Мониторинг ресурсов\n\n" + cpuLine + "\n" + memLine
+
+		return c.Send(finalOutput)
 	})
 
 	b.Start()
